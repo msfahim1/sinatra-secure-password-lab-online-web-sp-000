c7dcd6041c7d4fb98e8a9f4353538bc923f1b43a
Michael Fahim <michael_fahim@yahoo.com>
2020-08-14 02:01:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,10 @@
-ActiveRecord::Schema.define(version: 20180813201014) do
+
+ActiveRecord::Schema.define(version: 20200420173548) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password"
     t.string "password_digest"
-    t.float  "balance"
   end
 
 end
